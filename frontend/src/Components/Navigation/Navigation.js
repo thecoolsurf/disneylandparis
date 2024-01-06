66813f7459aa4e1ae074c596fcf5c97df5a6d347
douin <hdouin@free.fr>
2024-01-06 15:40:17 +0100

    update disneylandparis

--- frontend/src/Components/Navigation/Navigation.js
+++ frontend/src/Components/Navigation/Navigation.js
@@ -18,19 +18,21 @@ export const Navigation = (props) => {
         return arr;
     }
     const buildMenus = buildDatasNav();
+    const [show0, setShow0] = useState('show');
     const [show1, setShow1] = useState('hide');
     const [show2, setShow2] = useState('hide');
     return (
         <div className={'navigation' + ' ' + props.bkgNav}>
             <div className="slider" onClick={(e) => {
-                
+                console.log(show0)
+                show0 === 'show' ? setShow0('hide') : setShow0('show');
             }}><i className="fa fa-bars"></i></div>
-            <div className="nav">
+            <div className={'nav '+show0}>
                 {buildMenus.map((p) => {
                     let show =  (p.id === 1) ? show1 : show2;
                     return (
-                        <div className={'nav-parent'}>
-                            <a id={p.id} href={'/park-' + p.slug} alt={p.name} className={'link show ' + props.bkgNav} onMouseOver={(e) => {
+                        <div key={p.id} className={'nav-parent'}>
+                            <a key={p.slug} href={'/park-' + p.slug} alt={p.name} className={'link show ' + props.bkgNav} onMouseOver={(e) => {
                                 if (show1 === 'hide') { setShow1('show'); setShow2('hide'); }
                                 if (show1 === 'show') { setShow1('hide'); setShow2('show'); }
                                 if (show2 === 'hide') { setShow1('hide'); setShow2('show'); }
@@ -39,7 +41,7 @@ export const Navigation = (props) => {
                             </a>
                             <div id={'child-'+p.id} className={'nav-childs '+show}>
                                 {p.childs.map((u) => {
-                                    return (<a className={props.bkgNav + ' link'} href={'/park/' + p.slug + '/univers/' + u.slug}>{u.name}</a>)
+                                    return (<a key={u.slug} className={'link'} href={'/park/' + p.slug + '/univers/' + u.slug}>{u.name}</a>)
                                 })}
                             </div>
                         </div>
