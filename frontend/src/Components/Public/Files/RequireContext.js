e3fa191291dfa99b74c913e709120755cba6d3a9
douin <hdouin@free.fr>
2024-02-07 13:38:23 +0100

    update disneylandparis

--- frontend/src/Components/Public/Files/RequireContext.js
+++ frontend/src/Components/Public/Files/RequireContext.js
@@ -12,4 +12,83 @@ export const RequireContext = () => {
     if (uri.includes('big-thunder-mountain')) {
         return require.context('../../../assets/images/attractions/park/big-thunder-mountain/img',true);
     }
+    if (uri.includes('blanche-neige-et-les-sept-nains')) {
+        return require.context('../../../assets/images/attractions/park/blanche-neige-et-les-sept-nains/img',true);
+    }
+    if (uri.includes('buzz-lightyear-last-blast')) {
+        return require.context('../../../assets/images/attractions/park/buzz-lightyear-last-blast/img',true);
+    }
+    if (uri.includes('casy-jr-le-petit-train-du-cirque')) {
+        return require.context('../../../assets/images/attractions/park/casy-jr-le-petit-train-du-cirque/img',true);
+    }
+    if (uri.includes('discovery-arcade-main-street-usa')) {
+        return require.context('../../../assets/images/attractions/park/discovery-arcade-main-street-usa/img',true);
+    }
+    if (uri.includes('disneyland-railroad')) {
+        return require.context('../../../assets/images/attractions/park/disneyland-railroad/img',true);
+    }
+    if (uri.includes('dumbo-the-flying-elephant')) {
+        return require.context('../../../assets/images/attractions/park/dumbo-the-flying-elephant/img',true);
+    }
+    if (uri.includes('frontierland-playground')) {
+        return require.context('../../../assets/images/attractions/park/frontierland-playground/img',true);
+    }
+    if (uri.includes('horse-drawn-streetcarts')) {
+        return require.context('../../../assets/images/attractions/park/horse-drawn-streetcarts/img',true);
+    }
+    if (uri.includes('its-a-small-world')) {
+        return require.context('../../../assets/images/attractions/park/its-a-small-world/img',true);
+    }
+    if (uri.includes('la-cabane-des-robinson')) {
+        return require.context('../../../assets/images/attractions/park/la-cabane-des-robinson/img',true);
+    }
+    if (uri.includes('la-galerie-de-la-belle-au-bois-dormant')) {
+        return require.context('../../../assets/images/attractions/park/la-galerie-de-la-belle-au-bois-dormant/img',true);
+    }
+    if (uri.includes('la-plage-des-pirates')) {
+        return require.context('../../../assets/images/attractions/park/la-plage-des-pirates/img',true);
+    }
+    if (uri.includes('la-taniere-du-dragon')) {
+        return require.context('../../../assets/images/attractions/park/la-taniere-du-dragon/img',true);
+    }
+    if (uri.includes('le-carrousel-de-lancelot')) {
+        return require.context('../../../assets/images/attractions/park/le-carrousel-de-lancelot/img',true);
+    }
+    if (uri.includes('le-chateau-de-la-belle-au-bois-dormant')) {
+        return require.context('../../../assets/images/attractions/park/le-chateau-de-la-belle-au-bois-dormant/img',true);
+    }
+    if (uri.includes('le-galion-des-pirates')) {
+        return require.context('../../../assets/images/attractions/park/le-galion-des-pirates/img',true);
+    }
+    if (uri.includes('le-passage-enchant-d-aladdin')) {
+        return require.context('../../../assets/images/attractions/park/le-passage-enchant-d-aladdin/img',true);
+    }
+    if (uri.includes('le-pays-des-contes-de-fees')) {
+        return require.context('../../../assets/images/attractions/park/le-pays-des-contes-de-fees/img',true);
+    }
+    if (uri.includes('les-jardins-feeriques-de-main-street-usa')) {
+        return require.context('../../../assets/images/attractions/park/les-jardins-feeriques-de-main-street-usa/img',true);
+    }
+    if (uri.includes('les-mysteres-du-nautilus')) {
+        return require.context('../../../assets/images/attractions/park/les-mysteres-du-nautilus/img',true);
+    }
+    if (uri.includes('les-voyages-de-pinocchio')) {
+        return require.context('../../../assets/images/attractions/park/les-voyages-de-pinocchio/img',true);
+    }
+    if (uri.includes('liberty-arcade')) {
+        return require.context('../../../assets/images/attractions/park/liberty-arcade/img',true);
+    }
+    if (uri.includes('mad-hatter-s-tea-cups')) {
+        return require.context('../../../assets/images/attractions/park/mad-hatter-s-tea-cups/img',true);
+    }
+    if (uri.includes('main-street-vehicles')) {
+        return require.context('../../../assets/images/attractions/park/main-street-vehicles/img',true);
+    }
+    if (uri.includes('mickey-et-son-orchestre-philharmagique')) {
+        return require.context('../../../assets/images/attractions/park/mickey-et-son-orchestre-philharmagique/img',true);
+    }
 }
+
+
+
+
