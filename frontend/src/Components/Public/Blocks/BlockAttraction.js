b769098de79c9c6dd3d886953a7de276fdd6177a
douin <hdouin@free.fr>
2024-02-11 11:57:57 +0100

    update disneylandparis

--- frontend/src/Components/Public/Blocks/BlockAttraction.js
+++ frontend/src/Components/Public/Blocks/BlockAttraction.js
@@ -6,16 +6,19 @@ export const BlockAttraction = (props) => {
         props.datas.map((a) => {
             return (
                 <>
-                    <BlockLabelName label="Categorie" name={a.cname} />
-                    <BlockLabelName label="Public" name={a.public} />
-                    <BlockLabelName label="Interet" name={a.interet_description} />
-                    <BlockLabelName label="Taille" name={a.height_description} />
-                    <BlockLabelName label="Evacuation" name={a.evacuation_description} />
-                    <BlockLabelName label="Sensation" name={a.sensory_description} />
-                    <BlockLabelName label="Handicap" name={a.handicap_description} />
+                    <BlockLabelName label="Categorie" icon="fa fa-cogs" name={a.cname} />
+                    <BlockLabelName label="Public" icon="fa fa-users" name={a.public} />
+                    <BlockLabelName label="Durée" icon="fa fa-clock-o" name={a.public} />
+                    <BlockLabelName label="Interet" icon="fa fa-comment" name={a.interet_description} />
+                    <BlockLabelName label="Taille" icon="fa fa-text-height" name={a.height_description} />
+                    <BlockLabelName label="Evacuation" icon="fa fa-life-ring " name={a.evacuation_description} />
+                    <BlockLabelName label="Sensation" icon="fa fa-heartbeat" name={a.sensory_description} />
+                    <BlockLabelName label="Handicap" icon="fa fa-wheelchair" name={a.handicap_description} />
                     <BlockBack back={props.back} />
                 </>
             )
         })
     )
 }
+
+
--- frontend/src/Components/Public/Blocks/BlockLabelName.js
+++ frontend/src/Components/Public/Blocks/BlockLabelName.js
@@ -1,10 +1,22 @@
 export const BlockLabelName = (props) => {
-    return (
+    if (props.icon) {
+        return (
         <li className="items-2">
+            <i class={'icon '+props.icon} aria-hidden="true"></i>
             <div className="item">
                 <div className="label">{props.label}</div>
                 <div className="name">{props.name}</div>
             </div>
         </li>
-    )
+        )
+    } else {
+        return (
+            <li className="items-2">
+                <div className="item">
+                    <div className="label">{props.label}</div>
+                    <div className="name">{props.name}</div>
+                </div>
+            </li>
+        )
+    }
 }
