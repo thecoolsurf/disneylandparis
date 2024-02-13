99cc3f3e0048e29f164731ec296aa629cee73d7b
douin <hdouin@free.fr>
2024-02-13 15:41:21 +0100

    update disneylansparis

--- backend/src/Model/Public/Attraction/AttractionById.js
+++ backend/src/Model/Public/Attraction/AttractionById.js
@@ -2,8 +2,9 @@
 
 const attraction_by_id = `
 SELECT 
-a.id AS aid, a.slug AS aslug, a.route AS aroute, a.name AS aname, a.public, a.description, a.pictures, a.movies, 
+a.id AS aid, a.slug AS aslug, a.route AS aroute, a.name AS aname, a.description, 
 c.name AS cname, 
+p.name AS public,
 e.name AS ename, e.level, e.description as evacuation_description,
 h.name AS hname, h.description AS height_description,
 s.name AS sname, s.description AS sensory_description,
@@ -11,11 +12,12 @@ hh.name AS hhname, hh.description AS handicap_description,
 i.name AS iname, i.description AS interet_description
 FROM attraction a
 JOIN category c ON c.id = a.id_category
-JOIN evacuation e ON e.id = a.id_evacuation
-JOIN height h ON h.id = a.id_height
-JOIN sensory s ON s.id = a.id_sensory
-JOIN handicap hh ON hh.id IN(a.handicaps)
-JOIN interest i ON i.id IN(a.interests)
+LEFT JOIN public p ON p.id = a.id_public
+LEFT JOIN evacuation e ON e.id = a.id_evacuation
+LEFT JOIN height h ON h.id = a.id_height
+LEFT JOIN sensory s ON s.id = a.id_sensory
+LEFT JOIN handicap hh ON hh.id IN(a.handicaps)
+LEFT JOIN interest i ON i.id IN(a.interests)
 WHERE a.id = ?
 `;
 
