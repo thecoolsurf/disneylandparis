5bcf429eb4ac595ab2b4b97a3bc63d607fab503d
douin <hdouin@free.fr>
2024-02-25 09:23:47 +0100

    update disneylandparis

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ function select(route, sql) {
 		} else if (req.query.ids) {
 			let entity = route.split('/')[1];
 			let ids = req.query.ids.toString();
-			knex.raw(`SELECT name, description FROM ${entity} WHERE id IN (${ids})`)
+			knex.raw(`SELECT * FROM ${entity} WHERE id IN (${ids})`)
 				.then(([rows, columns]) => {
 					res.json(rows);
 				})
@@ -72,39 +72,25 @@ function cleanInputs(object) {
 	return object;
 }
 
-function update(route, sql) {
+function transaction(route, sql) {
 	let entity = route.split('/')[3];
+	let type = route.split('/')[2];
 	let url = `http://localhost:3000/admin/entity/collection/${entity}`;
+	let params = '';
 	server.post(route, (req, res, next) => {
-		let postDatas = cleanInputs(req.body);
-		let datas = jsonToArray(postDatas);
-		knex.raw(sql, datas).then(([rows, columns]) => {
-			res.redirect(url);
-		})
-	});
-	return server;
-}
-
-function insert(route, sql) {
-	let entity = route.split('/')[3];
-	let url = `http://localhost:3000/admin/entity/collection/${entity}`;
-	server.post(route, (req, res, next) => {
-		let postDatas = cleanInputs(req.body);
-		let datas = jsonToArray(postDatas);
-		datas.pop(); // for id
-		knex.raw(sql, datas).then(([rows, columns]) => {
-			res.redirect(url);
-		})
-	});
-	return server;
-}
-
-function deleting(route, sql) {
-	let entity = route.split('/')[3];
-	let url = `http://localhost:3000/admin/entity/collection/${entity}`;
-	server.post(route, (req, res, next) => {
-		let id = req.body.id;
-		knex.raw(sql, id).then(([rows, columns]) => {
+		if (type === 'insert') {
+			let postDatas = cleanInputs(req.body);
+			params = jsonToArray(postDatas);
+			params.pop(); // for id
+		}
+		if (type === 'update') {
+			let postDatas = cleanInputs(req.body);
+			params = jsonToArray(postDatas);
+		}
+		if (type === 'delete') {
+			params = req.body.id;
+		}
+		knex.raw(sql, params).then(([rows, columns]) => {
 			res.redirect(url);
 		})
 	});
@@ -115,30 +101,25 @@ function deleting(route, sql) {
 /* PUBLIC */
 
 /* menus */
-const nav_attractions_univers_parks = require('./Model/Public/Navigation/AttractionsAndUniversAndParks.js');
-select("/navigation", nav_attractions_univers_parks);
-
+const navigation = require('./Model/Public/Navigation/AttractionsAndUniversAndParks.js');
+select("/navigation", navigation);
 /* home page */
 const home = require('./Model/Public/Home/Datas.js');
 select("/home", home);
-
 /* park page */
 const park = require('./Model/Public/Park/ParkById.js');
 select("/park_by_id", park);
-
 /* univers page */
 const univers = require('./Model/Public/Univers/UniversById.js');
 select("/univers_by_id", univers);
 const attractions_by_univers = require('./Model/Public/Attraction/AttractionsByUnivers.js');
 select("/attractions_by_univers", attractions_by_univers);
-
 /* attraction page */
 const attraction_by_id = require('./Model/Public/Attraction/AttractionById.js');
 select("/attraction_by_id", attraction_by_id);
 select("/handicap", null);
 select("/interest", null);
 select("/premieraccess", null);
-
 /* find attraction page */
 const attraction_find = require('./Model/Public/Attraction/FindAttraction.js');
 select("/find_attraction", attraction_find);
@@ -151,9 +132,9 @@ for (const entity of entities) {
 	const folder = entity.charAt(0).toUpperCase() + entity.slice(1, entity.length);
 	select(`/admin/collection/${entity}`, require(`./Model/Admin/Entities/${folder}/Collection.js`));
 	select(`/admin/form/${entity}`, require(`./Model/Admin/Entities/${folder}/ById.js`));
-	update(`/admin/update/${entity}`, require(`./Model/Admin/Entities/${folder}/Update.js`));
-	insert(`/admin/insert/${entity}`, require(`./Model/Admin/Entities/${folder}/Insert.js`));
-	deleting(`/admin/delete/${entity}`, require(`./Model/Admin/Entities/${folder}/Delete.js`));
+	transaction(`/admin/update/${entity}`, require(`./Model/Admin/Entities/${folder}/Update.js`));
+	transaction(`/admin/insert/${entity}`, require(`./Model/Admin/Entities/${folder}/Insert.js`));
+	transaction(`/admin/delete/${entity}`, require(`./Model/Admin/Entities/${folder}/Delete.js`));
 }
 
 const attributes = require('../src/Tables/Attributes.js');
@@ -161,9 +142,9 @@ for (const attribute of attributes) {
 	const folder = attribute.charAt(0).toUpperCase() + attribute.slice(1, attribute.length);
 	select(`/admin/collection/${attribute}`, require(`./Model/Admin/Attributes/${folder}/Collection.js`));
 	select(`/admin/form/${attribute}`, require(`./Model/Admin/Attributes/${folder}/ById.js`));
-	update(`/admin/update/${attribute}`, require(`./Model/Admin/Attributes/${folder}/Update.js`));
-	insert(`/admin/insert/${attribute}`, require(`./Model/Admin/Attributes/${folder}/Insert.js`));
-	deleting(`/admin/delete/${attribute}`, require(`./Model/Admin/Attributes/${folder}/Delete.js`));
+	transaction(`/admin/update/${attribute}`, require(`./Model/Admin/Attributes/${folder}/Update.js`));
+	transaction(`/admin/insert/${attribute}`, require(`./Model/Admin/Attributes/${folder}/Insert.js`));
+	transaction(`/admin/delete/${attribute}`, require(`./Model/Admin/Attributes/${folder}/Delete.js`));
 }
 
 /* ************************************************************************************************** */
