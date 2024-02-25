cd0117b1cbddb2717160017f271b24bd6bf8838e
douin <hdouin@free.fr>
2024-02-25 16:15:13 +0100

    update disneylandparis

--- frontend/src/Components/Public/Document/CreatedAt.js
+++ frontend/src/Components/Public/Document/CreatedAt.js
@@ -1,13 +1,21 @@
 export const CreatedAt = (props) => {
     const date = new Date(props.date);
+    console.log(date);
+    let options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    }
     const day = date.getDay();
     const year = date.getFullYear();
-    const month = date.toLocaleDateString('fr',{month:'long'});
+    const created = date.toLocaleDateString('fr',options).split(' ');
+    console.log(created);
     return (
         <div className="dates">
             <div className="created">Création</div>
-            <div className="day">{day}</div>
-            <div className="month">{month}</div>
+            <div className="day">{created[1]}</div>
+            <div className="month">{created[2]}</div>
             <div className="year">{year}</div>
         </div>
     )
