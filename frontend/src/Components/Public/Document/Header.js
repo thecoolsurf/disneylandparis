a3d86efa726e532f0e7c501b7b3a47bf5c3e9872
douin <hdouin@free.fr>
2024-02-26 11:40:57 +0100

    update disneylandparis

--- frontend/src/Components/Public/Document/Header.js
+++ frontend/src/Components/Public/Document/Header.js
@@ -8,8 +8,8 @@ export const Header = () => {
         <header className={'header img-' + bkgHeader}>
             <div className="block-title">
                 <a className="title" href="/" alt="Home">
-                    <div className="item1">Rencontre</div>
-                    <div className="item2"><div className="mickey"></div>ta Souris</div>
+                    <div className="item1">Rencontre<div className="mickey"></div></div>
+                    <div className="item2">ta Souris</div>
                     <div className="item3">à Disneyland Paris</div>
                 </a>
             </div>
