2b00c6d009476449959780cbbaf2aa7f60b9fc66
douin <hdouin@free.fr>
2024-03-12 11:50:35 +0100

    update disneylandparis

--- frontend/src/Pages/Admin/Connexion.js
+++ frontend/src/Pages/Admin/Connexion.js
@@ -1,19 +1,17 @@
-import '../../assets/css/admin/admin.css'
+import '../../assets/css/admin/admin.css';
+import { Text } from '../../Components/Admin/Form/Inputs/Text.js';
+import { Password } from '../../Components/Admin/Form/Inputs/Password.js';
+import { Hidden } from '../../Components/Admin/Form/Inputs/Hidden.js';
 
 export const AdminConnexion = () => {
     return (
         <div className="admin">
             <form className="form" action={'http://localhost:80/admin/connexion'} method="POST" encType="application/x-www-form-urlencoded">
                 <fieldset>
-                    <legend>CONNEXION</legend>
-                    <p className="item">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" id="email" name="email" defaultValue="" />
-                    </p>
-                    <p className="item">
-                        <label htmlFor="password">Password</label>
-                        <input type="text" id="password" name="password" defaultValue="" />
-                    </p>
+                    <legend>Admin</legend>
+                    <Text name="email" value="" />
+                    <Password name="password" value="" />
+                    <Hidden name="token" value="" />
                     <input type="submit" value="Connexion" />
                 </fieldset>
             </form>
